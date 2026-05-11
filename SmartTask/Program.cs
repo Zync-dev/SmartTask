fd9398415a327dd03443074decc409579dd53099
Zync <Lukasjuulnielsen@gmail.com>
2026-05-11 21:13:31 +0200

    Railway 2

--- SmartTask/Program.cs
+++ SmartTask/Program.cs
@@ -30,6 +30,12 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
